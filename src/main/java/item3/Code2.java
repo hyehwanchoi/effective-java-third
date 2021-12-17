d847817b1ca4d1efa96cab895427338310437707
chh2644@gmail.com <gpghkschl12#>
2021-12-18 00:22:54 +0900

    feat : item3 in chapter2

--- src/main/java/item3/Code2.java
+++ src/main/java/item3/Code2.java
@@ -1,5 +1,7 @@
 package item3;
 
+// private 생성자나 열거 타입으로 싱글턴임을 보증하라
+// 2. 정적 팩터리 방식의 싱글턴
 public class Code2 {
   private static final Code2 INSTANCE = new Code2();
 
---
+++ src/main/java/item3/Code3.java
@@ -0,0 +1,11 @@
+package item3;
+
+// private 생성자나 열거 타입으로 싱글턴임을 보증하라
+// 3. 열거 타입 방식의 싱글턴
+public enum Code3 {
+    INSTANCE;
+
+    public static Code3 getInstance() {
+        return INSTANCE;
+    }
+}
