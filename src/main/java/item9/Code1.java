6d8a0862c3d0ad20ae4a2ea556cbea5157fa50d5
chh2644@gmail.com <gpghkschl12#>
2021-12-19 14:12:50 +0900

    feat : item5,6,7,9 in chapter2

---
+++ src/main/java/item9/Code1.java
@@ -0,0 +1,25 @@
+package item9;
+
+import java.io.*;
+
+// try~finally 보다는 try~with~resources를 사용하라.
+public class Code1 {
+
+    public StringBuilder readFile(String dir) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (
+                FileInputStream is = new FileInputStream(dir);
+                BufferedInputStream bis = new BufferedInputStream(is)
+        ) {
+            int data;
+            while ((data = bis.read()) != -1) {
+                stringBuilder.append(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder;
+    }
+}
